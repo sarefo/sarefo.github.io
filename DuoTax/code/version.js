a0d5f724f8c109e1a89e3eab667f618440cc3865
Sameer Revathi <sarefo@gmail.com>
2024-07-11 15:38:59 +0800

    tried to improve alt text before loading

--- DuoTax/code/version.js
+++ DuoTax/code/version.js
@@ -1,5 +1,5 @@
 // version.js
-const version = '1.2.1';
+const version = '1.2.2';
 
 function updateVersion() {
     const noCacheFiles = document.querySelectorAll('link[rel="stylesheet"], link[rel="preload"], script[src], link[rel="manifest"]');
