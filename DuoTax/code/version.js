1429580692546cd2452a33970adfaa924ce2d2c6
Sameer Revathi <sarefo@gmail.com>
2024-07-12 03:57:28 +0800

    added heart symbol

--- DuoTax/code/version.js
+++ DuoTax/code/version.js
@@ -1,5 +1,5 @@
 // version.js
-const version = '1.2.8';
+const version = '1.2.9';
 
 function updateVersion() {
     const noCacheFiles = document.querySelectorAll('link[rel="stylesheet"], link[rel="preload"], script[src], link[rel="manifest"]');
