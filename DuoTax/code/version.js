7fb504007d8775d2808af32033e8ce60c3bbf417
Sameer Revathi <sarefo@gmail.com>
2024-07-12 15:17:55 +0800

    version!

--- DuoTax/code/version.js
+++ DuoTax/code/version.js
@@ -1,5 +1,5 @@
 // version.js
-const version = '1.2.11';
+const version = '1.2.12';
 
 function updateVersion() {
     const noCacheFiles = document.querySelectorAll('link[rel="stylesheet"], link[rel="preload"], script[src], link[rel="manifest"]');
