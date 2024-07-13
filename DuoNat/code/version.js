8f8df559ed6d091ae86abe4bfbab3144cff75665
Sameer Revathi <sarefo@gmail.com>
2024-07-13 18:52:23 +0800

    changed folder name :(

--- DuoNat/code/version.js
+++ DuoNat/code/version.js
@@ -1,5 +1,5 @@
 // version.js
-const version = '1.3.21';
+const version = '1.3.22';
 
 function updateVersion() {
     const noCacheFiles = document.querySelectorAll('link[rel="stylesheet"], link[rel="preload"], script[src], link[rel="manifest"]');
