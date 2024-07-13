0e1306e1045b8dbe5ddb26a4490dfd9c78adebf6
Sameer Revathi <sarefo@gmail.com>
2024-07-14 01:49:22 +0800

    moved css to two files

--- DuoNat/code/version.js
+++ DuoNat/code/version.js
@@ -1,5 +1,5 @@
 // version.js
-const version = '1.3.32';
+const version = '1.3.33';
 
 function updateVersion() {
     const noCacheFiles = document.querySelectorAll('link[rel="stylesheet"], link[rel="preload"], script[src], link[rel="manifest"]');
