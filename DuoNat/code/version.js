695de17c43c80a045403e40cb1fbb7658b707bbb
Sameer Revathi <sarefo@gmail.com>
2024-07-13 22:31:32 +0800

    big overlay message

--- DuoNat/code/version.js
+++ DuoNat/code/version.js
@@ -1,5 +1,5 @@
 // version.js
-const version = '1.3.26';
+const version = '1.3.27';
 
 function updateVersion() {
     const noCacheFiles = document.querySelectorAll('link[rel="stylesheet"], link[rel="preload"], script[src], link[rel="manifest"]');
