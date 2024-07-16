62f79832831c51ecec227e5c6e409cbfb0fe6804
Sameer Revathi <sarefo@gmail.com>
2024-07-17 00:46:31 +0800

    font bit bigger

--- DuoNat/code/version.js
+++ DuoNat/code/version.js
@@ -1,5 +1,5 @@
 // version.js
-const version = '1.4.0';
+const version = '1.4.1';
 
 function updateVersion() {
     const noCacheFiles = document.querySelectorAll('link[rel="stylesheet"], link[rel="preload"], script[src], link[rel="manifest"]');
