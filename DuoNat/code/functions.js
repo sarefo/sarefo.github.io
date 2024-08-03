097154190ec8883e36c08e1eef406e5b6eb891d8
Sameer Revathi <sarefo@gmail.com>
2024-08-04 02:59:26 +0800

    Update functions.js

--- DuoNat/code/functions.js
+++ DuoNat/code/functions.js
@@ -13,6 +13,7 @@ import utils from './utils.js';
 
 (function () {
 
+
     // Set the log level based on your config
     logger.setLevel(config.debug ? LogLevel.DEBUG : LogLevel.INFO);
     document.getElementById('version-id').innerHTML = `Version: <i>${document.lastModified}</i>`;
