56bfa6cb5203ab706792852d08c8e4e1f68d59ec
Sameer Revathi <sarefo@gmail.com>
2024-08-04 02:57:50 +0800

    cache busting

--- DuoNat/code/version.js
+++ DuoNat/code/version.js
@@ -1,4 +1,4 @@
-const version = '1.4.29';
+const version = '1.4.30';
 
 function updateVersion() {
     const noCacheFiles = document.querySelectorAll('link[rel="stylesheet"], link[rel="preload"], script[src], link[rel="manifest"]');
