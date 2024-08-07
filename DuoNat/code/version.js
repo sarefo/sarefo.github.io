404ec9de934f36b5c4f2bb49c8cc78eec0c153c7
Sameer Revathi <sarefo@gmail.com>
2024-08-08 00:23:39 +0800

    version 1.5

--- DuoNat/code/version.js
+++ DuoNat/code/version.js
@@ -1,4 +1,4 @@
-const version = '1.4.36'; // Increment the version number
+const version = '1.5.0'; // Increment the version number
 
 function updateVersion() {
     // Update CSS, JS, and manifest files
