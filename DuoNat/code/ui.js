be236c5db687b09614e2d4fa214c2a784c471b78
Sameer Revathi <sarefo@gmail.com>
2024-08-11 11:04:07 +0800

    restored stabilized search screen

--- DuoNat/code/ui.js
+++ DuoNat/code/ui.js
@@ -104,18 +104,27 @@ const ui = {
         }
     },
 
-    updateFilterSummary() {
-        // This method should already exist, but make sure it handles empty filters correctly
+    updateFilterSummary: function () {
         const mapContainer = document.querySelector('.filter-summary__map');
         const tagsContainer = document.querySelector('.filter-summary__tags');
 
         if (mapContainer) {
-            mapContainer.innerHTML = ''; // Clear the map
-            createNonClickableWorldMap(mapContainer, new Set()); // Create an empty map
+            // Only redraw the map if the selected ranges have changed
+            const currentRanges = JSON.stringify(gameState.selectedRanges);
+            if (this.lastDrawnRanges !== currentRanges) {
+                mapContainer.innerHTML = '';
+                const selectedContinents = new Set(gameState.selectedRanges.map(abbr => getFullContinentName(abbr)));
+                createNonClickableWorldMap(mapContainer, selectedContinents);
+                this.lastDrawnRanges = currentRanges;
+            }
         }
 
         if (tagsContainer) {
-            tagsContainer.innerHTML = '<span class="filter-summary__no-tags">No active tags</span>';
+            tagsContainer.innerHTML = gameState.selectedTags.length > 0
+                ? gameState.selectedTags
+                    .map(tag => `<span class="filter-summary__tag">${tag}</span>`)
+                    .join('')
+                : '<span class="filter-summary__no-tags">No active tags</span>';
         }
     },
 
