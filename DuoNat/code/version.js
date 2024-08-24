12c4e59566006f6bd0a266c706634d1728c164f4
Sameer Revathi <sarefo@gmail.com>
2024-08-24 13:12:09 +0800

    testing

--- DuoNat/code/version.js
+++ DuoNat/code/version.js
@@ -1,4 +1,4 @@
-const version = '1.5.32'; // Increment the version number
+const version = '1.5.33'; // Increment the version number
 
 function updateUrlParams(element, attributeName) {
     const url = new URL(element[attributeName], window.location.href);
