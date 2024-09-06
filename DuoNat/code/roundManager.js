1db6c58cfec1cfa24eba82c30885fa5b9f6549e4
Sameer Revathi <sarefo@gmail.com>
2024-09-06 15:38:33 +0800

    Update roundManager.js

--- DuoNat/code/roundManager.js
+++ DuoNat/code/roundManager.js
@@ -29,38 +29,49 @@ const roundManager = {
                 };
                 //logger.debug("pair, pairData:",pairData.pair, pairDataWithImages);
 
-                const imageData = await this.getAndProcessImages(pairDataWithImages);
+               let  imageData = await this.getAndProcessImages(pairDataWithImages);
+
+               // TODO this definition changes the behavior!
+               imageData = {
+                    taxonImage1URL: pairData.image1URL,
+                    taxonImage2URL: pairData.image2URL,
+                    taxonImage1: pairData.pair.taxonA,
+                    taxonImage2: pairData.pair.taxonB,
+                    //randomized: false // We'll assume it's not randomized at this point
+                };
+
                 
                 this.setObservationURLs(imageData);
 
                 ui.prepareImagesForLoading();
                 //await roundManager.setupRoundComponents.setupRound(pairData.pair, imageData);
 
-            const { taxonImage1URL, taxonImage2URL, randomized, taxonImage1, taxonImage2 } = imageData;
+                const { taxonImage1URL, taxonImage2URL, randomized, taxonImage1, taxonImage2 } = imageData;
 
-            // Load images
-            await Promise.all([
-                roundManager.setupRoundComponents.loadImage(state.getElement('image1'), taxonImage1URL),
-                roundManager.setupRoundComponents.loadImage(state.getElement('image2'), taxonImage2URL)
-            ]);
+                // Load images
+                await Promise.all([
+                    roundManager.setupRoundComponents.loadImage(state.getElement('image1'), taxonImage1URL),
+                    roundManager.setupRoundComponents.loadImage(state.getElement('image2'), taxonImage2URL)
+                ]);
 
-            // Setup name tiles and world maps
-            const [nameTileData, worldMapData] = await Promise.all([
-                roundManager.setupRoundComponents.setupNameTiles(pairData.pair, randomized, taxonImage1, taxonImage2),
-                roundManager.setupRoundComponents.setupWorldMaps(pairData.pair, randomized, taxonImage1, taxonImage2)
-            ]);
+                // Setup name tiles and world maps
+                const [nameTileData, worldMapData] = await Promise.all([
+                    roundManager.setupRoundComponents.setupNameTiles(pairData.pair, randomized, taxonImage1, taxonImage2),
+                    roundManager.setupRoundComponents.setupWorldMaps(pairData.pair, randomized, taxonImage1, taxonImage2)
+                ]);
 
-            // Update game state
-            state.updateGameStateForRound(pairData.pair, imageData, nameTileData);
+                // Update game state
+                state.updateGameStateForRound(pairData.pair, imageData, nameTileData);
 
-            // Apply world map data
-            worldMap.createWorldMap(state.getElement('image1Container'), worldMapData.continents1);
-            worldMap.createWorldMap(state.getElement('image2Container'), worldMapData.continents2);
+                // Apply world map data
+                worldMap.createWorldMap(state.getElement('image1Container'), worldMapData.continents1);
+                worldMap.createWorldMap(state.getElement('image2Container'), worldMapData.continents2);
 
                 await ui.updateUIAfterSetup();
 
                 await this.fadeInNewImages();
-        await hintSystem.updateAllHintButtons();
+                await hintSystem.updateAllHintButtons();
+
             } catch (error) {
                 errorHandling.handleSetupError(error);
             } finally {
@@ -96,21 +107,16 @@ const roundManager = {
                 logger.error('Invalid pairData received in getImages');
                 throw new Error('Invalid pairData: pair is undefined');
             }
-            const { pair, preloadedImages } = pairData;
-
-            /*if (isNewPair && preloadedImages) {
-                //logger.debug(`Using preloaded images for pair: ${pair.taxonA} / ${pair.taxonB}`);
-                return { taxonA: preloadedImages.taxonA, taxonB: preloadedImages.taxonB };
-            }*/
+            const { pair } = pairData;
 
             const preloadedRoundImages = preloader.roundPreloader.getPreloadedImagesForNextRound();
             if (preloadedRoundImages && preloadedRoundImages.taxonA && preloadedRoundImages.taxonB) {
-                //logger.debug(`Using preloaded round images for pair: ${pair.taxonA} / ${pair.taxonB}`);
+                logger.debug(`Using preloaded round images for pair: ${pair.taxonA} / ${pair.taxonB}`);
                 preloader.roundPreloader.clearPreloadedImagesForNextRound();
                 return { taxonA: preloadedRoundImages.taxonA, taxonB: preloadedRoundImages.taxonB };
             }
 
-            //logger.debug(`Fetching new images for pair: ${pair.taxonA} / ${pair.taxonB}`);
+            logger.debug(`Fetching new images for pair: ${pair.taxonA} / ${pair.taxonB}`);
             return {
                 taxonA: await preloader.imageLoader.fetchDifferentImage(pair.taxonA, null),
                 taxonB: await preloader.imageLoader.fetchDifferentImage(pair.taxonB, null)
