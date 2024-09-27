97677b72e30643b02fb763a75b9e737c7296d76a
Stefan Obenauer <sarefo@gmail.com>
2024-09-27 21:55:39 +0800

    shortened cache update time to 10 days

--- DuoNat/code/cache.js
+++ DuoNat/code/cache.js
@@ -117,8 +117,8 @@ class DuoNatCache {
 }
 
 isCacheValid(lastUpdated) {
-    // Consider cache valid for 30 days
-    return Date.now() - lastUpdated < 24 * 60 * 60 * 1000 * 30;
+    // Consider cache valid for 10 days
+    return Date.now() - lastUpdated < 24 * 60 * 60 * 1000 * 10;
 
 }
 }
